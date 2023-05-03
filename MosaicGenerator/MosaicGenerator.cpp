fcd2c9526e2582bf8e0fb02276ba0955f9660f0d
Tabitha Roemish <v-taroem@microsoft.com>
2023-05-02 18:55:23 -0700

    Add project files.

---
+++ MosaicGenerator/MosaicGenerator.cpp
@@ -0,0 +1,18 @@
+//MosaicGenerator.cpp - inputs image and outputs an image with a mosaic tranformation. 
+// Aishwarya Pani and Tabitha Roemish
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+//main
+//preconditions:
+//postconditions:
+int main(int argc, char* argv[]) {
+
+
+}
